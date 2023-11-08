3a03504718fc3a6ec754c938070d3f3b938ed141
PurpleNurps <seanlunt1991@gmail.com>
2023-11-08 18:51:39 +0000

    initialised react practice app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import data from "./data.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
+function Header() {
+  return (
+    <>
+      <img src={logo} width="100px" alt="Logo" />
+      <h1 className="title">React Flashcards</h1>
+      <p>🧠 Expand Your React Knowledge, One Flashcard at a Time 🐌</p>
+    </>
+  );
+}
+
+function Main() {
+  return (
+    <section className="main">
+<Form />
+      <Flashcards/>
+    </section>
+  )
+}
+
+function Form() {
+  return (
+    <form>
+      <label for="question">Question: </label>
+      <input type="text" />
+      <label for="answer">Answer: </label>
+      <input type="text" />
+      <button>Add</button>
+    </form>
+  );
+}
+ 
+function Flashcards() {
+  return <Flashcard />
+}
+
+function Flashcard() {
+  return (
+    <section className="flashcard-container">
+      {data.map((id) => {
+        return (
+          <div className="flashcard" key={id}>
+            <p>🤔</p>
+            <p>{id.question}</p>
+          </div>
+        );
+      })}
+    </section>
+  );
+}
+
+function Footer() {
+return (<section className="footer">
+  <p>Built with React</p>
+  <img src={logo} width="25px" alt="Logo" />
+  </section>
+)
+}
 export default App;
